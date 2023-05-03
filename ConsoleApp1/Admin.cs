0dc7d20268d0c127c60e059a9a5c4ca054398e00
Elgun <novruzluqyds20@gmail.com>
2023-05-03 05:42:53 +0400

    Add project files.

---
+++ ConsoleApp1/Admin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Admin
+    {
+        public static int ID { get; set; } = 0;
+        public string? Username { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        
+    }
+}
---
+++ ConsoleApp1/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class User
+    {
+
+        // => Fields of user
+        public static int ID { get; set; } = 0;
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public string? Email { get; set; } 
+        public string? Password { get; set; }
+
+        // => Constructor of user
+        public User(string? name, string? surname, string? email, string? password)
+        {
+            ID++;
+            Name = name;
+            Surname = surname;
+            Email = email;
+            Password = password;
+        }
+
+    }
+}
